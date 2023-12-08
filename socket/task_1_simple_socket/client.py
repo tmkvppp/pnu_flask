dfa11f8fade28f66101a731970105d60ffc082d4
ritaaa3476@gmail.com <ritaaa3476@gmail.com>
2023-12-08 23:28:56 +0200

    lab_1_task1.2 make the server respond to everything (echo)

--- socket/task_1_simple_socket/client.py
+++ socket/task_1_simple_socket/client.py
@@ -5,6 +5,10 @@ PORT = 8080
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    msg = input('Enter something: ')
-    message = msg.encode('utf-8')
-    s.send(message)
+    while True:
+        msg = input('Enter something: ')
+        message = msg.encode('utf-8')
+        s.send(message)
+
+        data = s.recv(1024)
+        print(f"Received from server: {data.decode('utf-8')}")
--- socket/task_1_simple_socket/server.py
+++ socket/task_1_simple_socket/server.py
@@ -1,5 +1,5 @@
 import socket
-import datetime
+import time
 
 HOST = socket.gethostbyname(socket.gethostname())
 PORT = 8080
@@ -7,7 +7,6 @@ PORT = 8080
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
-    print(f"Server listening on {HOST}:{PORT}")
     conn, addr = s.accept()
     with conn:
         print(f"Connected by {addr}")
@@ -15,7 +14,10 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             data = conn.recv(1024)
             if not data:
                 break
-            decoded_data = data.decode('utf-8')
-            print(f"Received from client: {decoded_data}")
-            current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            print(f"Received at: {current_time}")
+            print(f"Received from client: {data.decode('utf-8')}")
+
+            time.sleep(5)
+            response = "Server received: " + data.decode('utf-8')
+            conn.send(response.encode('utf-8'))
+
+print("Connection with client closed.")
